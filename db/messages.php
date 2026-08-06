aaf5d7340fd6c815328735f9c21c873d10cc4bca
哈勃-慈含 <16178283@qq.com>
2026-08-06 23:04:36 +0800

    缺省支持web、email、app通知

--- db/messages.php
+++ db/messages.php
@@ -2,5 +2,11 @@
 // This file is part of Moodle - http://moodle.org/
 
 $messageproviders = array(
-    'mentions' => array(),
+    'mentions' => array(
+        'defaults' => [
+            'popup' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_ENABLED,
+            'email' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_ENABLED,
+            'airnotifier' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_ENABLED,
+        ],
+    ),
 );
